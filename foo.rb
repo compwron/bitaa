a4db1fb3e714e140c134e59424286804cc0158e8
compwron <compiledwrong@gmail.com>
2015-08-30 10:17:59 -0700

    the real answer using xor

--- foo.rb
+++ foo.rb
@@ -2,14 +2,13 @@ a = (0..10).map {|i| rand(1000)}
 b = a.shuffle
 missing_number = b.pop
 
-def find_missing_number(a, b)
-  sa = a.inject(&:+)
-  sb = b.inject(&:+)
-  sa - sb
+def find_missing_number(a, b, num=0)
+    a.each {|i| num ^= i}
+    b.each {|i| num ^= i}
+    num
 end
 
 result = find_missing_number(a, b)
 puts result == missing_number
 puts result
 puts missing_number
-
